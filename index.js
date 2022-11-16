3ba9e5c8ab5ee90dab44138bdf04a67abe8be66f
MoinulIslam7 <moinul180180@gmail.com>
2022-11-16 16:22:02 +0600

    bookings added in database

--- index.js
+++ index.js
@@ -10,21 +10,42 @@ app.use(express.json());
 
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://<username>:<password>@cluster0.0tydy0p.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.0tydy0p.mongodb.net/?retryWrites=true&w=majority`;
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    client.close();
-});
+
+
+async function run() {
+  try {
+    const appointmentOptionCollection = client.db("doctorsPortal").collection("appointmentOptions");
+    const bookingsCollection = client.db("doctorsPortal").collection("bookings");
+
+    app.get('/appointmentOptions', async (req, res) => {
+      const query = {};
+      const options = await appointmentOptionCollection.find(query).toArray();
+      res.send(options)
+    });
+
+    app.post('/bookings', async (req, res) => {
+      const booking = req.body;
+      console.log(booking);
+      const result = await bookingsCollection.insertOne(booking);
+      res.send(result)
+    })
+  }
+  finally {
+
+  }
+}
+run().catch(console.log);
+
 
 
 
 app.get('/', async (req, res) => {
-    res.send("doctors portal server is running");
+  res.send("doctors portal server is running");
 })
 app.listen(port, () => {
-    console.log("doctors portal running on port ", port);
+  console.log("doctors portal running on port ", port);
 })
 
-// doctorsPortal
