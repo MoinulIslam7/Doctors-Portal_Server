7eb2589e3e8786a01e2affc18a4ba9bbe8dcdfe1
MoinulIslam7 <moinul180180@gmail.com>
2022-11-22 00:49:02 +0600

    price added in database

--- index.js
+++ index.js
@@ -94,6 +94,7 @@ async function run() {
         {
           $project: {
             name: 1,
+            price: 1,
             slots: 1,
             booked: {
               $map: {
@@ -107,6 +108,7 @@ async function run() {
         {
           $project: {
             name: 1,
+            price: 1,
             slots: {
               $setDifference: ['$slots', '$booked']
             }
@@ -222,6 +224,18 @@ async function run() {
       res.send(result);
     })
 
+    // temporary to update price field on appointment options
+    // app.get('/addPrice', async (req, res) => {
+    //   const filter = {};
+    //   const options = {upsert: true}
+    //   const updateDoc = {
+    //     $set: {
+    //       price: 99
+    //     }
+    //   }
+    //   const result = await appointmentOptionCollection.updateMany(filter, updateDoc, options);
+    //   res.send(result);
+    // })
   }
   finally {
 
